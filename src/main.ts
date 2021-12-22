47baace5d99a244208fdf01fe9c66fac82de59d6
ryokohbato <ryokohbato@gmail.com>
2021-12-22 15:13:37 +0900

    🎨

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ const main = async () => {
     return;
   }
 
-  const date_hour = (argv["_"][0] as number);
+  const date_hour = argv["_"][0] as number;
   let duration = 0;
 
   switch (date_hour) {
@@ -46,15 +46,15 @@ const main = async () => {
   let items: any[] = [];
   const channels: any[] = [
     // 並び順に意味はない
-    "UCuTAXTexrhetbOe3zgskJBQ",         // 日テレNEWS
-    "UCGCZAYq5Xxojl_tSXcVJhiQ",         // ANNnewsCH
-    "UC6AG81pAkf6Lbi_1VC5NmPA",         // TBS NEWS
-    "UCoQBJMzcwmXrRSHBFAlTsIw",         // FNNプライムオンライン
-    "UCv7_krlrre3GQi79d4guxHQ",         // 読売テレビニュース
-    "UCkKJhKO73xF1pK5h9R82ZGQ",         // MBS NEWS
-    "UCkKVQ_GNjd8FbAuT6xDcWgg",         // テレ東BIZ
-    "UC1zYGo1jIIjMVDTLL3dydow",         // SankeiNews
-    "UCd6GEK664CTEWRZda7Fu7Lg",         // テレビ大阪ニュース
+    "UCuTAXTexrhetbOe3zgskJBQ", // 日テレNEWS
+    "UCGCZAYq5Xxojl_tSXcVJhiQ", // ANNnewsCH
+    "UC6AG81pAkf6Lbi_1VC5NmPA", // TBS NEWS
+    "UCoQBJMzcwmXrRSHBFAlTsIw", // FNNプライムオンライン
+    "UCv7_krlrre3GQi79d4guxHQ", // 読売テレビニュース
+    "UCkKJhKO73xF1pK5h9R82ZGQ", // MBS NEWS
+    "UCkKVQ_GNjd8FbAuT6xDcWgg", // テレ東BIZ
+    "UC1zYGo1jIIjMVDTLL3dydow", // SankeiNews
+    "UCd6GEK664CTEWRZda7Fu7Lg", // テレビ大阪ニュース
     // TODO: 朝日新聞社のチャンネルIDが分からん
   ];
 
@@ -67,10 +67,11 @@ const main = async () => {
     while (pageToken != null) {
       try {
         result = await axios.get(
-          `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channels[key]}&order=date&key=${(
-            await getKeys()
-          ).youtube
-          }&publishedAfter=${Date2RFC3339Format(publishedLimitTime)}&maxResults=50&pageToken=${pageToken}`
+          `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${
+            channels[key]
+          }&order=date&key=${(await getKeys()).youtube}&publishedAfter=${Date2RFC3339Format(
+            publishedLimitTime
+          )}&maxResults=50&pageToken=${pageToken}`
         );
 
         pageToken = result.data.nextPageToken;
@@ -79,7 +80,12 @@ const main = async () => {
         items = [...items, ...result.data.items];
       } catch (error: any) {
         // 気付ければどうでもいいのでこの辺は適当
-        await postText(`<@ryokohbato>\njp-media-news: 情報の取得に失敗しました。\n${JSON.stringify(error?.response?.data)}`, "#ryokohbato-dev-log-zatsu");
+        await postText(
+          `<@ryokohbato>\njp-media-news: 情報の取得に失敗しました。\n${JSON.stringify(
+            error?.response?.data
+          )}`,
+          "#ryokohbato-dev-log-zatsu"
+        );
         return;
       }
     }
@@ -95,7 +101,8 @@ const main = async () => {
       new Promise((resolve) =>
         setTimeout(async () => {
           await postText(
-            `${item.snippet.title}\nhttps://www.youtube.com/watch?v=${item.id.videoId}`, "#jp-media-news"
+            `${item.snippet.title}\nhttps://www.youtube.com/watch?v=${item.id.videoId}`,
+            "#jp-media-news"
           );
           resolve;
         }, index * 1000)
